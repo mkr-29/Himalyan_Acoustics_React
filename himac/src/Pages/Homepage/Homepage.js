87d270e00185e485670360e47b13ff396a6c64ed
Manas Bajpai <97174936+MacWeTT@users.noreply.github.com>
2023-01-24 10:20:50 +0530

    Footer and From Owner

--- himac/src/Pages/Homepage/Homepage.js
+++ himac/src/Pages/Homepage/Homepage.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import './Homepage.css'
 import Header from './Components/Header/Header'
 import Landing from './Components/Landing/Landing'
 import Services from './Components/Services/Services'
+import Footer from './Components/Footer/Footer'
+import FromOwner from './Components/FromOwner/FromOwner'
+import './Homepage.css'
 
 export default function Homepage() {
   return (
     <div>
-        <Header/>
-        <Landing/>
-        <Services/>
+      <Header />
+      <Landing />
+      <Services />
+      <FromOwner />
+      <Footer />
     </div>
   )
 }
