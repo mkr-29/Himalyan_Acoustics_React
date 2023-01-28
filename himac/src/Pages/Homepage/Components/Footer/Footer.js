91ca0d9ba6fa422755cb18604a7eedebe0abda6b
Mayank Kumar <mayank.k9802@gmail.com>
2023-01-28 09:33:22 +0530

    C

--- himac/src/Pages/Homepage/Components/Footer/Footer.js
+++ himac/src/Pages/Homepage/Components/Footer/Footer.js
@@ -1,42 +1,74 @@
-import React from 'react';
-import './Footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import { animateScroll as scroll } from "react-scroll";
+import { Link as LNK } from "react-scroll";
 
 function Footer() {
-    return (
-        <footer>
-            <h1 class="footer_head">
-                Himalayan
-                <strong class="footer_head_r"> Acoustics </strong>
-            </h1>
-            <div class="flinks">
-                <a onclick="scroll_to_top()" class="foot_link" href="http://" >
-                    Home
-                </a>
-                <a onclick="smooth_scroll_to_id('prod_range');" class="foot_link" href="http://">
-                    Products
-                </a>
-                <a onclick="smooth_scroll_to_id('why-us')" class="foot_link" href="http://">
-                    About Us
-                </a>
-                <a onclick="smooth_scroll_to_id('con_us')" class="foot_link" href="http://">
-                    Contact
-                </a>
-            </div>
+  return (
+    <footer>
+      <h1 class="footer_head">
+        Himalayan
+        <strong class="footer_head_r"> Acoustics </strong>
+      </h1>
+      <div class="flinks">
+        <Link to="/" onClick={() => scroll.scrollToTop()} class="foot_link">
+          Home
+        </Link>
+        <LNK
+          to="prod_range"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={1000}
+          class="foot_link"
+        >
+          Products
+        </LNK>
+        <LNK
+          to="why-us"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={1000}
+          class="foot_link"
+        >
+          About Us
+        </LNK>
+        <LNK
+          to="con_us"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={1000}
+          class="foot_link"
+        >
+          Contact
+        </LNK>
+      </div>
 
-            <div class="ser_off_socials con_socials footer-social_icons">
-                <a href="http://"><img class="con_ic" src="Icons/icon _facebook_.svg" alt="" /></a>
-                <a href="http://"><img class="con_ic" src="Icons/icon _linkedin.svg" alt="" /></a>
-                <a href="http://"><img class="con_ic" src="Icons/icon _twitter.svg" alt="" /></a>
-                <a href="http://"><img class="con_ic" src="Icons/icon _instagram.svg" alt="" /></a>
-            </div>
+      <div class="ser_off_socials con_socials footer-social_icons">
+        <a href="">
+          <img class="con_ic" src="Icons/icon _facebook_.svg" alt="" />
+        </a>
+        <a href="">
+          <img class="con_ic" src="Icons/icon _linkedin.svg" alt="" />
+        </a>
+        <a href="">
+          <img class="con_ic" src="Icons/icon _twitter.svg" alt="" />
+        </a>
+        <a href="">
+          <img class="con_ic" src="Icons/icon _instagram.svg" alt="" />
+        </a>
+      </div>
 
-            <div class="footer-contact">username@email.com | +91 452 482 5615</div>
-            <div class="footer-copyright">
-                © 2022 Himalyan Accounts (Kool Pack and Allied Industries) • All Rights
-                Reserved
-            </div>
-        </footer>
-    )
+      <div class="footer-contact">username@email.com | +91 452 482 5615</div>
+      <div class="footer-copyright">
+        © 2022 Himalyan Accounts (Kool Pack and Allied Industries) • All Rights
+        Reserved
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
